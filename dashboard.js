28849c2ef81085de55d5b3e5423b9ddf5eb6556b
fatemeh shahsavan <shahsavan051@yahoo.com>
2025-09-18 12:00:59 +0330

    authorization pages

---
+++ dashboard.js
@@ -0,0 +1,8 @@
+import {getCookie} from "./utils/cookie.js"
+const init=()=>{
+    const cookie=getCookie()
+    if(!cookie){
+     location.assign("index.html")
+    }
+}
+document.addEventListener("DOMContentLoaded",init)
