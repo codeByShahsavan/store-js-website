e4ed5ce97b53f3b55ca83091cf9c466a7215cbad
fatemeh shahsavan <shahsavan051@yahoo.com>
2025-09-21 23:06:52 +0330

    cart list

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,12 @@
 
 import authHandler from "./utils/authorization.js"
 import { getData } from "./utils/httpReq.js"
+import { shortenText } from "./utils/stringFunc.js"
 
 
 const mainContent=document.getElementById("container")
+const cartContent=document.getElementById("container-cart")
+
 const  logoutButton=document.querySelector("button")
 
 const renderUsers=(users)=>{
@@ -40,11 +43,59 @@ const renderUsers=(users)=>{
 
 }
 
+const renderCarts=(carts,users,products)=>{
+    cartContent.innerHTML=""
+    
+    carts.forEach((cart)=>{
+       const user=users.find(user=>user.id===cart.userId)
+       let jsxUser=`
+        <div id="card">
+       <h3>${user.id}</h3>
+   
+       <div>
+       <p><i class="fa-solid fa-paperclip"></i>Username:</p>
+       <span>${user.username}</span>
+       </div>
+     
+       `
+  let totalCartPrice=0
 
+      cart.products.forEach(item => {
+      const product = products.find(product => product.id === item.productId);
+      totalCartPrice+=item.quantity*product.price
+     jsxUser+=`
+       <div>
+       <p><i class="fa-solid  fa-cart-arrow-down"></i>product:</p>
+           <img src=${product.image} alt=${product.title}/>
+       <span>${shortenText(product.title)}</span>
+        <p><i class="fa-solid fa-clipboard"></i>quantity:</p>
+        <span>${item.quantity}</span>
+
+               <p><i class="fa-solid fa-money-check"></i>price:</p>
+        <span>${item.quantity*product.price}</span>
+       </div>
+   
+       `
+
+    })
+       jsxUser += `
+       <div>
+            <p><i class="fa-solid fa-money-check"></i>Total Cart Price:</p>
+      <span> ${totalCartPrice}$</span>
+      </div>
+      </div>
+      `
+      cartContent.innerHTML+=jsxUser
+    })
+
+}
 const init=async()=>{
     authHandler()
     const users=await getData("users")
      renderUsers(users)
+     const carts=await getData("carts")
+     const products=await getData("products")
+     renderCarts(carts,users,products)
 
 }
 
@@ -52,5 +103,6 @@ const logoutHandler=()=>{
     document.cookie="token=; max-age=0"
     location.assign("index.html")
 }
+
 document.addEventListener("DOMContentLoaded",init)
 logoutButton.addEventListener("click",logoutHandler)
